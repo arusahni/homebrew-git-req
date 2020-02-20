ea590ede93c3fb693f1728bbf61755220d52d73b
Aru Sahni <arusahni@gmail.com>
2020-02-19 22:15:15 -0500

    v2.2.0-rc.2

--- git-req.rb
+++ git-req.rb
@@ -1,18 +1,18 @@
 class GitReq < Formula
   desc "Check out merge requests from GitLab/GitHub repositories with ease!"
   homepage "https://arusahni.github.io/git-req/"
-  url "https://github.com/arusahni/git-req/archive/v2.2.0-rc.0.tar.gz"
-  sha256 "deac9a0942f2ad4d9294b1b44fa7c018cd0a15bd23f7769779720e21ef643c8c"
+  url "https://github.com/arusahni/git-req/archive/v2.2.0-rc.2.tar.gz"
+  sha256 "d4929b9215bc40236ebee9b5b495aab9cea7ac624dabe04a16af53948768555f"
 
   depends_on "rust" => :build
 
   def install
     system "cargo", "install", "--root", prefix, "--path", "."
     man.mkpath
-    man1.install "target/release/git-req.man"
+    man1.install "target/release/git-req.1"
   end
 
   test do
-    assert_match /git-req 2.2.0-rc.0/, shell_output("#{bin}/git-req --version")
+    assert_match /git-req 2.2.0-rc.2/, shell_output("#{bin}/git-req --version")
   end
 end
